4c682f046e9debce7603694fe1e1e27c9356250f
Shota <shotakatamadze7@gmail.com>
2025-03-19 00:53:20 +0400

    init

---
+++ app/model/Base.js
@@ -0,0 +1,7 @@
+Ext.define('ProgressTracker.model.Base', {
+    extend: 'Ext.data.Model',
+
+    schema: {
+        namespace: 'ProgressTracker.model'
+    }
+});
---
+++ app/model/Personnel.js
@@ -0,0 +1,7 @@
+Ext.define('ProgressTracker.model.Personnel', {
+    extend: 'ProgressTracker.model.Base',
+
+    fields: [
+        'name', 'email', 'phone'
+    ]
+});
